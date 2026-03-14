2ae1beb97eb2ad0c34dd2fd5395544b0ac0dffb5
nelson981026 <nechan1983@gmail.com>
2026-03-14 20:31:21 +0800

    0314

---
+++ apcs/202007_3圓環出口q.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, m, used = 0, tar, l;
+    cin >> n >> m;
+    vector<int> point(n), task(m), pfs(n);
+    for (auto &x : point) {
+        cin >> x;
+    }
+    for (auto &x : task) {
+        cin >> x;
+    }
+    pfs[0] = point[0];
+    for (int i = 1; i < n; ++i) {
+        pfs[i] = point[i] + pfs[i - 1];
+    }
+    for (auto x : task) {
+        tar = (x + used - 1) % pfs[n - 1] + 1;
+        auto i = lower_bound(pfs.begin(), pfs.end(), tar);
+        used = *i;
+        l = i - pfs.begin();
+    }
+    printf("%d\n", (l + 1) % n);
+    return 0;
+}
